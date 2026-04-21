2ff0c4689e2dba539e67ee5c6324af795cac0dd1
inflame-ue <inflamokil@gmail.com>
2026-04-21 11:05:27 +0300

    feat: implement MakeJWT and ValidateJWT;

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -20,4 +23,41 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return match, fmt.Errorf("the password and hash do not match: %v", err)
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	signedToken, err := token.SignedString(tokenSecret)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign the generated token: %v", err)
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("failed to parse the token with claims: %v", err)
+	}
+
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("failed to get the subject: %v", err)
+	}
+
+	parsedUUID, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("faield to parse the subject's UUID: %v", err)
+	}
+
+	return parsedUUID, nil
 }
